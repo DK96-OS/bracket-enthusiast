cb3db837f50e9cf27f075974706a42f9e2c92e47
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2023-09-10 15:40:19 -0400

    Test: *u BracketEnthusiastCodeStringTest - add test cases

--- main/src/test/java/brackets/BracketEnthusiastCodeStringTest.kt
+++ main/src/test/java/brackets/BracketEnthusiastCodeStringTest.kt
@@ -3,6 +3,7 @@ package brackets
 import org.junit.After
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /** Testing BracketEnthusiast on Code Strings.
@@ -11,7 +12,7 @@ class BracketEnthusiastCodeStringTest {
 
 	private var mInstance: BracketEnthusiast? = null
 
-	private val codeString1: String = """
+	private val codeStringInput1: String = """
 		package brackets
 		
 		class CodeStringClass {
@@ -24,6 +25,18 @@ class BracketEnthusiastCodeStringTest {
 		}
 	""".trimIndent()
 
+	private var curlyEnthusiast1 = BracketEnthusiast(
+		BracketType.CURLY, codeStringInput1
+	)
+
+	private var parenthesisEnthusiast1 = BracketEnthusiast(
+		BracketType.PARENTHESIS, codeStringInput1
+	)
+
+	private var squareEnthusiast1 = BracketEnthusiast(
+		BracketType.SQUARE, codeStringInput1
+	)
+
 	@After
 	fun testCleanup() {
 		mInstance = null
@@ -31,68 +44,88 @@ class BracketEnthusiastCodeStringTest {
 
 	@Test
 	fun testOpeners_CodeString1_Curly() {
-		mInstance = BracketEnthusiast(
-			BracketType.CURLY,
-			codeString1
-		)
 		assertArrayEquals(
 			intArrayOf(40, 61, 132),
-			mInstance!!.openers
+			curlyEnthusiast1.openers
 		)
 	}
 
 	@Test
 	fun testOpeners_CodeString1_Parenthesis() {
-		mInstance = BracketEnthusiast(
-			BracketType.PARENTHESIS,
-			codeString1
-		)
 		assertArrayEquals(
 			intArrayOf(58, 72, 109),
-			mInstance!!.openers
+			parenthesisEnthusiast1.openers
 		)
 	}
 
 	@Test
 	fun testOpeners_CodeString1_Square() {
-		mInstance = BracketEnthusiast(
-			BracketType.SQUARE,
-			codeString1
-		)
-		assertNull(mInstance!!.openers)
+		assertNull(squareEnthusiast1.openers)
 	}
 
 	@Test
 	fun testClosers_CodeString1_Curly() {
-		mInstance = BracketEnthusiast(
-			BracketType.CURLY,
-			codeString1
-		)
 		assertArrayEquals(
 			intArrayOf(90, 158, 160),
-			mInstance!!.closers
+			curlyEnthusiast1.closers
 		)
 	}
 
 	@Test
 	fun testClosers_CodeString1_Parenthesis() {
-		mInstance = BracketEnthusiast(
-			BracketType.PARENTHESIS,
-			codeString1
-		)
 		assertArrayEquals(
 			intArrayOf(59, 87, 122),
-			mInstance!!.closers
+			parenthesisEnthusiast1.closers
 		)
 	}
 
 	@Test
 	fun testClosers_CodeString1_Square() {
-		mInstance = BracketEnthusiast(
-			BracketType.SQUARE,
-			codeString1
+		assertNull(squareEnthusiast1.closers)
+	}
+
+	@Test
+	fun testAreBracketsBalanced_CodeString1_Curly_ReturnsTrue() {
+		assertTrue(
+			curlyEnthusiast1.areBracketsBalanced
 		)
-		assertNull(mInstance!!.closers)
+	}
+
+	@Test
+	fun testAreBracketsBalanced_CodeString1_Parenthesis_ReturnsTrue() {
+		assertTrue(
+			parenthesisEnthusiast1.areBracketsBalanced
+		)
+	}
+
+	@Test
+	fun testAreBracketsBalanced_CodeString1_Square_ReturnsTrue() {
+		assertTrue(
+			squareEnthusiast1.areBracketsBalanced
+		)
+	}
+
+	@Test
+	fun testGetBracketPairs_CodeString1_Curly() {
+		assertArrayEquals(
+			intArrayOf(40, 160, 61, 90, 132, 158),
+			curlyEnthusiast1.getBracketPairs()
+		)
+	}
+
+	@Test
+	fun testGetBracketPairs_CodeString1_Parenthesis() {
+		assertArrayEquals(
+			intArrayOf(58, 59, 72, 87, 109, 122),
+			parenthesisEnthusiast1.getBracketPairs()
+		)
+	}
+
+	@Test
+	fun testGetBracketPairs_CodeString1_Square() {
+		assertNull(
+			squareEnthusiast1.getBracketPairs()
+		)
 	}
 
 }
