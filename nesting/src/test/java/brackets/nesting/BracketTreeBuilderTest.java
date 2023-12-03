8a2027c36f04cb4bad9a76df98bd833b63e50ead
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2023-12-02 20:06:30 -0500

    Tests: *c BracketTreeBuilderTest

---
+++ nesting/src/test/java/brackets/nesting/BracketTreeBuilderTest.java
@@ -0,0 +1,90 @@
+package brackets.nesting;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/** Testing the BracketTree Builder class.
+ * The builder is setup with an initial root node.
+ */
+public final class BracketTreeBuilderTest {
+
+	int openRoot;
+	int closeRoot;
+	BracketTreeBuilder mInstance;
+
+	@Before
+	public void testSetup() {
+		openRoot = 2;
+		closeRoot = 10;
+		mInstance = new BracketTreeBuilder();
+		//
+		assertTrue(
+			mInstance.addNode(openRoot, closeRoot)
+		);
+	}
+
+	@Test
+	public void testInitialCondition() {
+		assertNotNull(
+			mInstance.root
+		);
+		assertEquals(
+			openRoot,
+			mInstance.root.first
+		);
+		assertEquals(
+			closeRoot,
+			mInstance.root.second
+		);
+	}
+
+	@Test
+	public void testAddNode_SameAsRoot_ReturnsFalse() {
+		assertFalse(
+			mInstance.addNode(
+				openRoot, closeRoot
+			)
+		);
+		assertNotNull(
+			mInstance.root
+		);
+	}
+
+	@Test
+	public void testAddNode_WithinRoot_ReturnsTrue() {
+		final int open = 4;
+		final int close = 8;
+		assertTrue(
+			mInstance.addNode(
+				open, close
+			)
+		);
+		final var root = mInstance.root;
+		assertNotNull(root);
+		// Check that the root contain the inner values
+		assertEquals(
+			1, root.countInternalNodes()
+		);
+		assertEquals(
+			open, root.getInternalNodes().get(0).first
+		);
+		assertEquals(
+			close, root.getInternalNodes().get(0).second
+		);
+	}
+
+	@Test
+	public void testClear_IsEmpty() {
+		mInstance.clear();
+		assertNull(
+			mInstance.root
+		);
+	}
+
+}
